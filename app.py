0998fa80b99db82d2b0c37ff04b91ff201225166
Ayushh <AyushMishra0228@outlook.com>
2023-12-19 15:02:10 +0530

    new commit

---
+++ app.py
@@ -0,0 +1,13 @@
+import sys 
+from PyQt5.QtWidgets import QApplication , QMainWindow
+from PyQt5.QtGui import QIcon
+def window():
+    app = QApplication(sys.argv)
+    win = QMainWindow()
+    win.setGeometry(200,300,1500,700)
+    win.setWindowTitle("Vista Keychain (password manager)")
+    win.setWindowIcon(QIcon("titleicon.jpg"))
+    win.show()
+    sys.exit(app.exec_())
+
+window()
